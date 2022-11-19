f6f8cae3fb5e5d0d9c3f942ae4975c55de5ab4c5
seungyoonLee <syoon624@naver.com>
2022-11-19 20:28:35 +0900

    feat: 기본 환경 세팅

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (env, options) => {
+  return {
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    entry: './src/app.js',
+    output: {
+      //path: '',
+      //filename: 'main.js',
+      publicPath: '/',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+        {
+          // 이미지 포멧: PNG, JP(E)G, GIF, SVG, WEBP
+          test: /\.png$/i,
+          use: ['file-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlPlugin({
+        template: './src/index.html',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: 'assets', to: '' }],
+      }),
+      new Dotenv({
+        systemvars: true,
+      }),
+    ],
+    devServer: {
+      port: 8080,
+      open: true,
+      historyApiFallback: true,
+    },
+  };
+};
