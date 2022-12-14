9df8b05d3497409130b08c3fc93582c91a5b8cfb
seungyoonLee <syoon624@naver.com>
2022-12-14 17:15:49 +0900

    refactor: role-attribute 적용

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,7 @@
 const nav = (element) => {
   const el = document.querySelector(element);
   return (el.innerHTML += /* html */ `
-    <ul class="nav-ul">
+    <ul role="navigation" class="nav-ul">
       <li id="/" class="nav-li"><a class="nav-a" href="/">전체</a></li>
       <li id="/done" class="nav-li"><a class="nav-a" href="/done">TODO <strong>✔️</strong> </a></li>
       <li id="/notdone" class="nav-li"><a class="nav-a" href="/notdone">TODO <strong>𝗫</strong></a></li>
--- src/components/option.js
+++ src/components/option.js
@@ -1,18 +1,18 @@
 const option = (element) => {
   const el = document.querySelector(element);
   return (el.innerHTML += /* html */ `
-      <ul class="option-ul hidden">
-        <li id='default' class="option-li">기본</li>
-        <li id='newest' class="option-li">최신 순으로 보기</li>
-        <li id='oldest' class="option-li">오래된 순으로 보기</li>
+      <ul role="tab" class="option-ul hidden">
+        <li role="tablist" id='default' class="option-li">기본</li>
+        <li role="tablist" id='newest' class="option-li">최신 순으로 보기</li>
+        <li role="tablist" id='oldest' class="option-li">오래된 순으로 보기</li>
         ${
           location.pathname === '/'
-            ? `<li id='filter' class="option-li">완료/미완료 구분해서 보기</li>`
+            ? `<li role="tablist" id='filter' class="option-li">완료/미완료 구분해서 보기</li>`
             : ''
         }
         ${
           location.pathname === '/trash'
-            ? `<li id='delete-all' class="option-li">휴지통 비우기</li>`
+            ? `<li role="tablist" id='delete-all' class="option-li">휴지통 비우기</li>`
             : ''
         }
       </ul>
